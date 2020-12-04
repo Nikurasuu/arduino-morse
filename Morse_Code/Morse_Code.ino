a46da6fe766b53e9427f73dfd99f4986e7fdec85
Niklas Malkusch <56842703+Nikurasuu@users.noreply.github.com>
2020-12-04 21:41:45 +0100

    update

--- Morse_Code/Morse_Code.ino
+++ Morse_Code/Morse_Code.ino
@@ -3,36 +3,108 @@ int low_counter;
 
 void read_letter(int number){
   if(number == 1){
-    Serial.print("h");
+    Serial.print("e");
   }
   if(number == 2){
-    Serial.print("a");
+    Serial.print("n");
   }
   if(number == 3){
-    Serial.print("l");
+    Serial.print("i");
   }
   if(number == 4){
-    Serial.print("o");
+    Serial.print("s");
   }
   if(number == 5){
+    Serial.print("r");
+  }
+  if(number == 6){
+    Serial.print("a");
+  }
+  if(number == 7){
+    Serial.print("t");
+  }
+  if(number == 8){
+    Serial.print("d");
+  }
+  if(number == 9){
+    Serial.print("h");
+  }
+  if(number == 10){
+    Serial.print("u");
+  }
+  if(number == 11){
+    Serial.print("l");
+  }
+  if(number == 12){
+    Serial.print("c");
+  }
+  if(number == 13){
+    Serial.print("g");
+  }
+  if(number == 14){
+    Serial.print("m");
+  }
+  if(number == 15){
+    Serial.print("o");
+  }
+  if(number == 16){
+    Serial.print("b");
+  }
+  if(number == 17){
+    Serial.print("w");
+  }
+  if(number == 18){
+    Serial.print("f");
+  }
+  if(number == 19){
+    Serial.print("k");
+  }
+  if(number == 20){
+    Serial.print("z");
+  }
+  if(number == 21){
+    Serial.print("p");
+  }
+  if(number == 22){
+    Serial.print("v");
+  }
+  if(number == 23){
+    Serial.print("j");
+  }
+  if(number == 24){
+    Serial.print("y");
+  }
+  if(number == 25){
+    Serial.println("x");
+  }
+  if(number == 26){
+    Serial.print("q");
+  }
+  if(number == 27){
+    Serial.print(" ");
+  }
+  if(number == 28){
     Serial.println(" ");
   }
 }
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Serial begin");
   pinMode(A1, INPUT);
 }
 
 void loop() {
-  if(analogRead(A1) > 1010){
-    while(analogRead(A1) > 1010){
+  if(analogRead(A1) > 1005){
+    while(analogRead(A1) > 1005){
       led_on_time++;
+      //Serial.println(led_on_time);
     }
-    if(led_on_time > 8000){
+    //Serial.println(led_on_time);
+    if(led_on_time > 240){
       read_letter(low_counter);
       low_counter = 0;
-    } else if(led_on_time > 2900) {
+    } else if(led_on_time > 94) {
       low_counter++;
     }
     led_on_time = 0;
--- Morse_Code_Sender/Morse_Code_Sender.ino
+++ Morse_Code_Sender/Morse_Code_Sender.ino
@@ -1,9 +1,12 @@
-#define pause 350
-#define pulse 350
-#define start 1000
+#define pause 12
+#define pulse 12
+#define start 20
 
-void blink_led(int counter){
-  for(int i = 0; i < counter; i++){
+char rx_byte = 0;
+String rx_str = "";
+
+void blink_led(int counter) {
+  for (int  i = 0; i < counter; i++) {
     digitalWrite(2, LOW);
     delay(pause);
     digitalWrite(2, HIGH);
@@ -11,31 +14,145 @@ void blink_led(int counter){
   }
 }
 
-void print_letter(int letter){
-  if (letter == "h"){
+void print_letter(String letter) {
+  Serial.print("sending: "); Serial.println(letter);
+  if (letter == "e") {
     digitalWrite(2, HIGH);
     delay(start);
     blink_led(1);
   }
-  if (letter == "a"){
+  if (letter == "n") {
     digitalWrite(2, HIGH);
     delay(start);
     blink_led(2);
   }
-  if (letter == "l"){
+  if (letter == "i") {
     digitalWrite(2, HIGH);
     delay(start);
     blink_led(3);
   }
-  if (letter == "o"){
+  if (letter == "s") {
     digitalWrite(2, HIGH);
     delay(start);
     blink_led(4);
   }
-  if (letter == "skip"){
+  if (letter == "r") {
     digitalWrite(2, HIGH);
     delay(start);
     blink_led(5);
+  }
+  if (letter == "a") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(6);
+  }
+  if (letter == "t") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(7);
+  }
+  if (letter == "d") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(8);
+  }if (letter == "h") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(9);
+  }
+  if (letter == "u") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(10);
+  }
+  if (letter == "l") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(11);
+  }
+  if (letter == "c") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(12);
+  }
+  if (letter == "g") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(13);
+  }
+  if (letter == "m") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(14);
+  }
+  if (letter == "o") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(15);
+  }
+  if (letter == "b") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(16);
+  }
+  if (letter == "w") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(17);
+  }
+  if (letter == "f") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(18);
+  }
+  if (letter == "k") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(19);
+  }
+  if (letter == "z") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(20);
+  }
+  if (letter == "p") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(21);
+  }if (letter == "v") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(22);
+  }
+  if (letter == "j") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(23);
+  }
+  if (letter == "y") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(24);
+  }
+  if (letter == "x") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(25);
+  }
+  if (letter == "q") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(26);
+  }
+  if (letter == " ") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(27);
+  }
+  if (letter == "skip") {
+    digitalWrite(2, HIGH);
+    delay(start);
+    blink_led(28);
   }
   digitalWrite(2, LOW);
   delay(pause);
@@ -47,10 +164,22 @@ void setup() {
 }
 
 void loop() {
-  print_letter("h");
-  print_letter("a");
-  print_letter("l");
-  print_letter("l");
-  print_letter("o");
-  print_letter("skip");
+  if (Serial.available() > 0) {
+    rx_byte = Serial.read();
+
+    if (rx_byte != '\n') {
+      rx_str += rx_byte;
+    }
+    else {
+      print_letter("skip");
+      for (auto x : rx_str)
+      {
+        String y = String(x);
+        print_letter(y);
+      }
+      print_letter("skip");
+      Serial.println(rx_str);
+      rx_str = "";
+    }
+  }
 }
